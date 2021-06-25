44c5554dbe6799fe0a9e71c9bc44cc8d54c2befe
Richarqu <richarque@gmail.com>
2021-06-25 11:23:22 +0100

    created data values in database and app

---
+++ CardTokenizationAPI/TokenProvider/DataValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CardTokenizationAPI.TokenProvider
+{
+    public class DataValues
+    {
+        public int count { get; set; }
+        public DataSet dataSet { get; set; }
+    }
+}
