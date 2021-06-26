096bfaff6c9d2a9187fd6fa65885230f379a6f70
Richarqu <richarque@gmail.com>
2021-06-26 01:31:24 +0100

    FEP connection successfully connected, plus FEP authentication params set for users

---
+++ CardTokenizationAPI/Auth/BasicAuthenticationAttribute.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace CardTokenizationAPI.Auth
+{
+    public class BasicAuthenticationAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            //if (actionContext.Request.Headers.Authorization == null)
+            //{
+            //    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            //}
+            //else
+            //{
+                //do client and secret authentication here for token generation
+                //string authenticationToken = actionContext.Request.Headers.Authorization.Parameter;
+                //string decodedAuthenticationToken = Encoding.UTF8.GetString(Convert.FromBase64String(authenticationToken));
+                //string[] usernamePasswordArray = decodedAuthenticationToken.Split(':');
+                //string username = usernamePasswordArray[0];
+                //string password = usernamePasswordArray[1];
+                //if (ClientValidation.Login(username, password))
+                if (ClientValidation.AuthenticateClient(actionContext.Request))
+                {
+                    //Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username), null);
+                    return;
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+            //}
+        }
+
+        private bool AuthorizeRequest(HttpActionContext actionContext)
+        {
+            var isAuthorized = false;
+            isAuthorized = ClientValidation.AuthenticateClient(actionContext.Request);
+            return isAuthorized;
+        }
+    }
+}
---
+++ CardTokenizationAPI/TokenProvider/FEPConn.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardTokenizationAPI.TokenProvider
+{
+    class FEPConn
+    {
+        public SqlConnection conn;
+        public SqlCommand sql;
+        public int num_rows;
+        public int returnValue;
+        public FEPConn(string query)
+        {
+            try
+            {
+                conn = new SqlConnection();
+                //conn.ConnectionString = "data source=172.25.31.4;initial catalog=postcard;persist security info=True;User ID=carduser;Password=C@rdusr1";
+                conn.ConnectionString = "data source=10.0.41.239;initial catalog=postcard;persist security info=True;User ID=olaniranqr;Password=Password12";
+                conn.Open();
+                sql = new SqlCommand();
+                sql.Connection = conn;
+                sql.CommandText = query;
+                sql.CommandType = CommandType.Text;
+            }
+            catch (Exception ex)
+            {
+                new ErrorLog(ex);
+            }
+        }
+        public void addparam(string key, object val)
+        {
+            sql.Parameters.AddWithValue(key, val);
+        }
+        public DataSet query(string tblName)
+        {
+            DataSet ds = new DataSet();
+            num_rows = 0;
+            try
+            {
+                SqlDataAdapter res = new SqlDataAdapter();
+                res.SelectCommand = sql;
+                res.TableMappings.Add("Table", tblName);
+                res.Fill(ds);
+                num_rows = ds.Tables[tblName].Rows.Count;
+            }
+            catch (Exception ex)
+            {
+                new ErrorLog(ex);
+            }
+
+            close();
+            return ds;
+        }
+        public int query()
+        {
+            SqlParameter prm = new SqlParameter();
+            prm.SqlDbType = SqlDbType.Int;
+            prm.Direction = ParameterDirection.ReturnValue;
+            sql.Parameters.Add(prm);
+            returnValue = 0;
+            int j = 0;
+            try
+            {
+                j = sql.ExecuteNonQuery();
+                returnValue = Convert.ToInt32(prm.Value);
+            }
+            catch (Exception ex)
+            {
+                new ErrorLog(ex);
+            }
+            close();
+            return j;
+        }
+        public DataSet select()
+        {
+            DataSet ds = new DataSet();
+            num_rows = 0;
+            try
+            {
+                SqlDataAdapter res = new SqlDataAdapter();
+                res.SelectCommand = sql;
+                res.TableMappings.Add("Table", "recs");
+                res.Fill(ds);
+                num_rows = ds.Tables["recs"].Rows.Count;
+            }
+            catch { }
+            close();
+            return ds;
+        }
+        public int delete()
+        {
+            int j = 0;
+            try
+            {
+                j = sql.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                new ErrorLog(ex);
+            }
+            close();
+            return j;
+        }
+        public int update()
+        {
+            int j = 0;
+            try
+            {
+                j = sql.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                new ErrorLog(ex);
+            }
+            close();
+            return j;
+        }
+        public object insert()
+        {
+            sql.CommandText += "; select @@IDENTITY ";
+            object j = 0;
+            try
+            {
+                j = sql.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                new ErrorLog(ex);
+            }
+            close();
+            return j;
+        }
+        public string selectScalar()
+        {
+            string j = "";
+            try
+            {
+                j = Convert.ToString(sql.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                new ErrorLog(ex);
+            }
+            close();
+            return j;
+        }
+        public void close()
+        {
+            conn.Close();
+        }
+    }
+}
